1a18f0eef3d7e45215039cb6b2d776356e53ad6b
andrewhamilton0 <acjts425@gmail.com>
2022-04-04 17:58:47 -0400

    Adds classes ColorWheel and FactBook. Adds multiple facts to app and changing colors

--- app/src/main/java/com/andrew/funfacts/MainActivity.java
+++ app/src/main/java/com/andrew/funfacts/MainActivity.java
@@ -2,13 +2,44 @@ package com.andrew.funfacts;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    private FactBook factBook = new FactBook();
+    private ColorWheel colorWheel = new ColorWheel();
+    // Declare view variables
+    private TextView factTextView;
+    private Button showFactButton;
+    private RelativeLayout relativeLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Assign views from layout file to corresponding variables
+        factTextView = findViewById(R.id.funFactTextView);
+        showFactButton = findViewById(R.id.showFactButton);
+        relativeLayout = findViewById(R.id.relativeLayout);
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                factTextView.setText(factBook.getFact());
+                int color = colorWheel.getColor();
+                relativeLayout.setBackgroundColor(color);
+                showFactButton.setTextColor(color);
+
+            }
+        };
+        showFactButton.setOnClickListener(listener);
+
+
     }
 }
